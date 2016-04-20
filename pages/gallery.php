dc29da2bcf6215ce5343373c7f2b4964f41b8be2
Frederick MARINHO <frmarinh@e3r2p17.42.fr>
2016-04-20 20:42:12 +0200

    Galerie

---
+++ pages/gallery.php
@@ -0,0 +1,11 @@
+<div class="middle_galerie">
+					<div id="galerie">
+						<a href=""><img class="img_gal" src="img/imgres.jpg"/></a>
+						<form style="text-align: right; margin-right: 10px; margin-top: 6px;">
+							<input type="button" value="x"></input>
+							<a href="" style="text-decoration: none;"><font style="margin-right: 600px; margin-top: -20px; text-decoration: none; display: inline-block;">&#128077</font></a>
+							<a href="" style="text-decoration: none;" ><font style="margin-right: 560px; margin-top: -26px; display: inline-block;">&#128078</font></a>
+						</form>
+					</div>
+					
+				</div>
